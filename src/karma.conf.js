6752dfa644a07d808d848b47204b9945e8de797d
Sharayu Shah <SharayuS@citiustech.com>
2018-08-29 16:47:55 +0530

    second commit

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -10,7 +10,9 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+        require('karma-coverage-istanbul-reporter'), // <- Add plugin
+      require('karma-junit-reporter'), // <- Add plugin
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -25,7 +27,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+   browsers: [ 'PhantomJS'],
     singleRun: false
   });
 };
